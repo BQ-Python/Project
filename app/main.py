1d666157195147836bf22ef50beb4836e4adcbe3
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 14:23:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,5 @@
 from fastapi import FastAPI
-from supabase import create_client
 from app.routes import loans_router, swaps_router, banks_router, kpi_router
-
-# Initialisation du client Supabase
-SUPABASE_URL = "https://gchxuxnhwkusymfbnjmj.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjaHh1eG5od2t1c3ltZmJuam1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyODAwMDIsImV4cCI6MjA3MTg1NjAwMn0.m2mC_kCzlPh_eknpqzQEKiNiuDc_BqZD7GT8UJLYROQ"
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Initialisation de l'application FastAPI
 app = FastAPI()
