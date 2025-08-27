33af0c46e35ebef788774a9f93b4d141db568882
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 09:42:43 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes import loan, swap, kpi
-
-app = FastAPI()
-
-app.include_router(loan.router, prefix="/loan")
-app.include_router(swap.router, prefix="/swap")
-app.include_router(kpi.router, prefix="/kpi")
