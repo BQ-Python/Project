a5ef2b466ac6ced14eb5e3e05e683d41a561d380
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 12:01:14 +0200

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,5 @@
-
+# /app/app/routes/__init__.py
+from .loans import router as loans_router
+from .swaps import router as swaps_router
+from .banks import router as banks_router
+from .kpi import router as kpi_router
