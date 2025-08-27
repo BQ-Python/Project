36a12dfb2bde9095a4f4f3c4f64947506e026660
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 16:14:51 +0200

    Update supabase_client.py

--- app/supabase_client.py
+++ app/supabase_client.py
@@ -1,3 +1,4 @@
+# app/supabase_client.py
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
@@ -6,5 +7,8 @@ load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
+
+if not url or not key:
+    raise RuntimeError("Les variables SUPABASE_URL ou SUPABASE_KEY sont manquantes.")
 
 supabase: Client = create_client(url, key)
