ab38536f2101ac8c234c76b282db2e1e3fff57a8
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 16:20:20 +0200

    Update supabase_client.py

--- app/supabase_client.py
+++ app/supabase_client.py
@@ -1,9 +1,6 @@
 # app/supabase_client.py
 import os
-from dotenv import load_dotenv
 from supabase import create_client, Client
-
-load_dotenv()
 
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
