5d9e978656deeab8f985377f6353a50740422346
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 10:04:13 +0200

    Update schemes.py

--- app/schemes.py
+++ app/schemes.py
@@ -1,1 +1,40 @@
+from pydantic import BaseModel
+from datetime import date
+
+class LoanBase(BaseModel):
+    currency: str
+    nominal: float
+    rate: float
+    start_date: date
+    maturity_date: date
+
+class LoanCreate(LoanBase): pass
+class Loan(LoanBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class SwapBase(BaseModel):
+    currency: str
+    nominal: float
+    start_date: date
+    maturity_date: date
+    spot_rate: float
+    forward_rate: float
+    bank_id: int
+
+class SwapCreate(SwapBase): pass
+class Swap(SwapBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class BankBase(BaseModel):
+    name: str
+
+class BankCreate(BankBase): pass
+class Bank(BankBase):
+    id: int
+    class Config:
+        orm_mode = True
 
