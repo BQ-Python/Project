82f5319560b84c2194bafe124603df9f4ccf1520
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 12:34:46 +0200

    Update banks.py

--- app/routes/banks.py
+++ app/routes/banks.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.database import SessionLocal
 from models import Bank
 from schemas import BankCreate, Bank
 
