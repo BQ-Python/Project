034ed34d02e84ebf1444d489d464811d797545ca
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 10:05:03 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from .routes import loans, swaps, banks, kpi
+from .database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(loans.router)
+app.include_router(swaps.router)
+app.include_router(banks.router)
+app.include_router(kpi.router)
 
