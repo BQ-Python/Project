820ec122b4c9e0ec62e04876c2978aa5103af041
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 12:13:27 +0200

    Update kpi.py

--- app/routes/kpi.py
+++ app/routes/kpi.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from database import SessionLocal
+from app.database import SessionLocal
 from models import Loan, Swap
 
 router = APIRouter()
