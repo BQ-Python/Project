54f1ed1217cb1a646f9920fb44e965b1258c1757
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-27 12:35:09 +0200

    Update loans.py

--- app/routes/loans.py
+++ app/routes/loans.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.database import SessionLocal
 from models import Loan
 from schemas import LoanCreate, Loan
 
