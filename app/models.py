e34a59afc6311cf7d1b5360ecd41c5656ef561e9
B.Q <140819894+BQ-Python@users.noreply.github.com>
2025-08-28 16:32:29 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,16 +2,6 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 
-class Loan(Base):
-    __tablename__ = "loans"
-    id = Column(Integer, primary_key=True, index=True)
-    currency = Column(String)
-    nominal = Column(Float)
-    rate = Column(Float)
-    start_date = Column(Date)
-    maturity_date = Column(Date)
-    payment_frequency = Column(String)  # ✅ Nouveau champ ajouté
-
 class Swap(Base):
     __tablename__ = "swaps"
     id = Column(Integer, primary_key=True, index=True)
@@ -23,6 +13,14 @@ class Swap(Base):
     forward_rate = Column(Float)
     bank_id = Column(Integer, ForeignKey("banks.id"))
     bank = relationship("Bank")
+
+    # Champs calculés
+    spot_value_eur = Column(Float)
+    forward_value_eur = Column(Float)
+    swap_points_eur = Column(Float)
+    mtm_eur = Column(Float)
+    total_days = Column(Integer)
+    remaining_days = Column(Integer)
 
 class Bank(Base):
     __tablename__ = "banks"
